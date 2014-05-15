eef20fa5a78bcfa744376d07c49f946033189846
Gulthor <jbmusso+github@gmail.com>
2014-05-15 12:21:47 +0200

    Rename event routes

--- mozcal/events/urls.py
+++ mozcal/events/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = patterns('',
   # Match
-  url(r'^(?P<slug>[a-z0-9-]+)$', views.one, name='event.one'),
-  url(r'^', views.all, name='event.all'),
+  url(r'^(?P<slug>[a-z0-9-]+)$', views.one, name='event_one'),
+  url(r'^', views.all, name='event_all'),
 )
