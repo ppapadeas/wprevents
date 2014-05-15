6f31cb8cad05f1713fbdef861c194385bc875ad9
Gulthor <jbmusso+github@gmail.com>
2014-05-15 17:19:48 +0200

    Clean up comment

--- mozcal/events/urls.py
+++ mozcal/events/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.defaults import url, patterns
 from . import views
 
 urlpatterns = patterns('',
-  # Match
   url(r'^(?P<slug>[a-z0-9-]+)$', views.one, name='event_one'),
   url(r'^', views.all, name='event_all'),
 )
