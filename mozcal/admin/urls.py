a3b73aefc6c5a411ecdfb5b6da54a3f6a7488786
Gulthor <jbmusso+github@gmail.com>
2014-05-16 15:16:11 +0200

    Add edit space route in admin section

--- mozcal/admin/urls.py
+++ mozcal/admin/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = patterns('',
   url(r'^events/$', views.events_list, name='event_all'),
   url(r'^events/(?P<id>\d+)/edit', views.event_edit, name='event_edit'),
+  url(r'^spaces/(?P<id>\d+)/edit', views.space_edit, name='space_edit'),
 )
--- mozcal/admin/views.py
+++ mozcal/admin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from mozcal.events.models import Event
+from mozcal.events.models import Event, Space
 
 
 def events_list(request):
@@ -13,3 +13,9 @@ def event_edit(request, id):
   event = get_object_or_404(Event, id=id)
 
   return render(request, 'edit_event.html', { 'event': event })
+
+
+def space_edit(request, id):
+  space = get_object_or_404(Space, id=id)
+
+  return render(request, 'edit_space.html', { 'space': space })
