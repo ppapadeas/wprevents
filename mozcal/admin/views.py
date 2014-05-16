7428c56f74012078e8c4bf270fd196e9dd2f3b32
Gulthor <jbmusso+github@gmail.com>
2014-05-16 17:57:47 +0200

    Forms no longer show errors on first load

--- mozcal/admin/views.py
+++ mozcal/admin/views.py
@@ -12,9 +12,10 @@ def events_list(request):
   return render(request, 'events.html', { 'events': events })
 
 
+#@see https://github.com/mozilla/remo/blob/master/remo/events/views.py#L148
 def event_edit(request, id):
   event = get_object_or_404(Event, id=id)
-  form = EventForm(request.POST, instance=event)
+  form = EventForm(request.POST or None, instance=event)
 
   if request.method == 'POST':
     if form.is_valid():
@@ -32,7 +33,7 @@ def spaces_list(request):
 
 def space_edit(request, id):
   space = get_object_or_404(Space, id=id)
-  form = SpaceForm(request.POST, instance=space)
+  form = SpaceForm(request.POST or None, instance=space)
 
   if request.method == 'POST':
     if form.is_valid():
