ffee056415e7d1d1bcd0bb0a1c42a506ab0cd6af
Gulthor <jbmusso+github@gmail.com>
2014-05-16 19:47:37 +0200

    Add fields properties to all form Meta classes

--- mozcal/admin/forms.py
+++ mozcal/admin/forms.py
@@ -6,8 +6,10 @@ from mozcal.events.models import Event, Space
 class EventForm(ModelForm):
   class Meta:
     model = Event
+    fields = ['title', 'space', 'start', 'end', 'areas', 'description', 'details']
 
 
 class SpaceForm(ModelForm):
   class Meta:
     model = Space
+    fields = ['name', 'address', 'address2', 'city', 'country', 'description', 'lat', 'lon']
