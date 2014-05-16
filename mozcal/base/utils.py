7b279565e1bdb71e14f41f9e9d8795ae2ed4ab02
Gulthor <jbmusso+github@gmail.com>
2014-05-16 19:14:12 +0200

    Add get_or_create_instance function

---
+++ mozcal/base/utils.py
@@ -0,0 +1,9 @@
+def get_or_create_instance(model_class, **kwargs):
+  """
+  Identical to get_or_create, expect instead of saving the new
+  object in the database, this just creates an instance.
+  """
+  try:
+    return model_class.objects.get(**kwargs), False
+  except model_class.DoesNotExist:
+    return model_class(**kwargs), True
