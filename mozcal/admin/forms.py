bc14f2df9c1e6fbc3c3abbf49b264feba0e6a8ab
Gulthor <jbmusso+github@gmail.com>
2014-05-19 12:53:11 +0200

    Add SplitDateTime widget

--- mozcal/admin/forms.py
+++ mozcal/admin/forms.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
+from django import forms
 from django.forms import ModelForm
 
+from datetimewidgets import SplitSelectDateTimeWidget
+
+from mozcal.base.utils import validate_datetime
 from mozcal.events.models import Event, Space
 
 
@@ -7,6 +13,31 @@ class EventForm(ModelForm):
   class Meta:
     model = Event
     fields = ['title', 'space', 'start', 'end', 'areas', 'description', 'details']
+    widgets = {
+      'start': SplitSelectDateTimeWidget(),
+      'end': SplitSelectDateTimeWidget()
+    }
+
+  def __init__(self, *args, **kwargs):
+    super(EventForm, self).__init__(*args, **kwargs)
+
+    self.initSplitSelectDateTimeWidget()
+
+  def initSplitSelectDateTimeWidget(self):
+    now = datetime.now()
+    start_year = min(getattr(self.instance.start, 'year', now.year),
+                       now.year - 1)
+    end_year = min(getattr(self.instance.end, 'year', now.year),
+                     now.year - 1)
+
+    self.fields['start'] = forms.DateTimeField(
+      widget=SplitSelectDateTimeWidget(
+          years=range(start_year, start_year + 10), minute_step=5),
+      validators=[validate_datetime])
+    self.fields['end'] = forms.DateTimeField(
+      widget=SplitSelectDateTimeWidget(
+          years=range(end_year, end_year + 10), minute_step=5),
+      validators=[validate_datetime])
 
 
 class SpaceForm(ModelForm):
--- mozcal/base/utils.py
+++ mozcal/base/utils.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def get_or_create_instance(model_class, **kwargs):
   """
   Identical to get_or_create, expect instead of saving the new
@@ -7,3 +10,16 @@ def get_or_create_instance(model_class, **kwargs):
     return model_class.objects.get(**kwargs), False
   except model_class.DoesNotExist:
     return model_class(**kwargs), True
+
+def validate_datetime(data, **kwargs):
+  """Validate that /data/ is of type datetime.
+
+  Used to validate DateTime form fields, to ensure that user select
+  a valid date, thus a date that can be converted to a datetime
+  obj. Example of invalid date is 'Sept 31 2012'.
+
+  """
+
+  if not isinstance(data, datetime):
+      raise ValidationError('Date chosen is invalid.')
+  return data
