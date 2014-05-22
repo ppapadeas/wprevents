7a0f3251dd3772e66af1c837585b6ea67d58ba45
Gulthor <jbmusso+github@gmail.com>
2014-05-22 16:48:57 +0200

    Fix indentation in Gulpfile.js

--- client/Gulpfile.js
+++ client/Gulpfile.js
@@ -11,52 +11,52 @@ function logError(error) {
   console.error(error.message);
 }
 
+function onChange(event) {
+  console.log('File', event.type +':', event.path);
+}
+
 gulp.task('scripts', function() {
   gulp.src('js/app.js')
-      .pipe(browserify({
-        debug: true
-      }))
-      .on('error', logError)
-      .pipe(size({ showFiles: true }))
-      .pipe(gulp.dest(staticDir + '/js'));
+    .pipe(browserify({
+      debug: true
+    }))
+    .on('error', logError)
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest(staticDir + '/js'));
 
   gulp.src('js/admin.js')
-      .pipe(browserify({
-        debug: true
-      }))
-      .on('error', logError)
-      .pipe(size({ showFiles: true }))
-      .pipe(gulp.dest(staticDir + '/js'));
+    .pipe(browserify({
+      debug: true
+    }))
+    .on('error', logError)
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest(staticDir + '/js'));
 });
 
 gulp.task('sass', function() {
   gulp.src('scss/main.scss')
-      .pipe(sass({
-        sourcemap: true
-      }))
-      .on('error', logError)
-      .pipe(size({ showFiles: true }))
-      .pipe(gulp.dest(staticDir + '/css'));
+    .pipe(sass({
+      sourcemap: true
+    }))
+    .on('error', logError)
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest(staticDir + '/css'));
 
   gulp.src('scss/admin.scss')
-      .pipe(sass({
-        sourcemap: true
-      }))
-      .on('error', logError)
-      .pipe(size({ showFiles: true }))
-      .pipe(gulp.dest(staticDir + '/css'));
+    .pipe(sass({
+      sourcemap: true
+    }))
+    .on('error', logError)
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest(staticDir + '/css'));
 });
 
 gulp.task('watch', function() {
-  function onChange(event) {
-    console.log('File', event.type +':', event.path);
-  }
-
   gulp.watch('js/**/*', ['scripts'])
-      .on('change', onChange);
+    .on('change', onChange);
 
   gulp.watch('scss/**/*', ['sass'])
-      .on('change', onChange);
+    .on('change', onChange);
 });
 
 gulp.task('default', ['dev']);
