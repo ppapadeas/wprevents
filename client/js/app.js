4460f413e3b3b4bf5d6fd04fc99a3caca0ca4e0d
sork <franck.git@gmail.com>
2014-05-22 18:36:44 +0200

    Add empty MapView class as an example of Backbone integration

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,15 @@
-console.log('app.js');
+var $ = require('jquery');
+var Backbone = require('backbone');
+
+Backbone.$ = $;
+
+var MapView = require('./views/map');
+
+
+var App = function() {
+  var map = new MapView();
+}
+
+$(function() {
+  var app = new App();
+});
---
+++ client/js/views/map.js
@@ -0,0 +1,10 @@
+var Backbone = require('backbone');
+var $ = require('jquery');
+
+var MapView = Backbone.View.extend({
+  initialize: function() {
+    // TODO
+  }
+});
+
+module.exports = MapView;
