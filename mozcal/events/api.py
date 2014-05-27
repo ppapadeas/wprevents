779cbd21b3ec3de0f2659751bf2b2c8886d67f66
Gulthor <jbmusso+github@gmail.com>
2014-05-27 20:41:36 +0200

    Use tastypie route when exporting events as CSV

--- mozcal/events/api.py
+++ mozcal/events/api.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tastypie.resources import ModelResource
 from models import Event
 
@@ -15,3 +16,14 @@ class EventResource(ModelResource):
     include_absolute_url = False
 
     serializer = MozcalSerializer(formats=['json', 'csv'])
+
+  def create_response(self, request, data, **response_kwargs):
+    """Add HTTP header to specify the filename of CSV exports."""
+    response = super(EventResource, self).create_response(request, data, **response_kwargs)
+
+    if self.determine_format(request) == 'text/csv':
+      today = datetime.now().date()
+      filename = today.strftime('events-export-%Y-%m-%d.csv')
+      response['Content-Disposition'] = 'filename="%s"' % filename
+
+    return response
