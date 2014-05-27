027cdb68192ddbdf8540eaee685124f59106c8db
Guigui <guillaume@littleworkshop.fr>
2014-05-27 17:38:54 +0200

    fix position of calender event tooltips

--- client/js/views/calendarevent.js
+++ client/js/views/calendarevent.js
@@ -8,12 +8,14 @@ var CalendarEventView = Backbone.View.extend({
 
   initialize: function() {
     this.$clone = '';
+    this.topBorder = this.$el.parents('tr').prevAll().length === 0 ? 0 : 1;
+    this.leftBorder = this.$el.parents('td').prevAll().length === 0 ? 0 : 1;
   },
 
   clone: function(e) {
     var $cell = this.$el.parents('td');
-    $cell.offset.y = this.$el.offset().top - $cell.offset().top;
-    $cell.offset.x = this.$el.offset().left - $cell.offset().left - 1; // we remove 1px to account for the cell border
+    $cell.offset.y = this.$el.offset().top - $cell.offset().top - this.topBorder;
+    $cell.offset.x = this.$el.offset().left - $cell.offset().left - this.leftBorder;
 
     var $clone = this.$el.clone();
     $clone.addClass('tooltip');
