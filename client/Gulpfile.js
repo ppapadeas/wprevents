2a1df15625af1c0826d4a07c12a4b3f3f17ed04e
Gulthor <jbmusso+github@gmail.com>
2014-05-28 14:47:29 +0200

    Add gulp build-dev and build-prod tasks

--- client/Gulpfile.js
+++ client/Gulpfile.js
@@ -16,54 +16,79 @@ function onChange(event) {
   console.log('File', event.type +':', event.path);
 }
 
-gulp.task('scripts', function() {
+// JavaScript
+gulp.task('scripts-prod', function() {
   gulp.src('js/app.js')
-    .pipe(browserify({
-      debug: true
-    }))
-    // .pipe(uglify())
+    .pipe(browserify())
+    .pipe(uglify())
     .on('error', logError)
     .pipe(size({ showFiles: true }))
     .pipe(gulp.dest(staticDir + '/js'));
 
   gulp.src('js/admin.js')
-    .pipe(browserify({
-      debug: true
-    }))
-    // .pipe(uglify())
+    .pipe(browserify())
+    .pipe(uglify())
     .on('error', logError)
     .pipe(size({ showFiles: true }))
     .pipe(gulp.dest(staticDir + '/js'));
 });
 
-gulp.task('sass', function() {
+gulp.task('scripts-dev', function() {
+  gulp.src('js/app.js')
+    .pipe(browserify({ debug: true }))
+    .on('error', logError)
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest(staticDir + '/js'));
+
+  gulp.src('js/admin.js')
+    .pipe(browserify({ debug: true }))
+    .on('error', logError)
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest(staticDir + '/js'));
+});
+
+// CSS
+gulp.task('sass-prod', function() {
   gulp.src('scss/main.scss')
-    .pipe(sass({
-      sourcemap: true
-    }))
+    .pipe(sass({ sourcemap: false }))
     .on('error', logError)
     .pipe(size({ showFiles: true }))
     .pipe(gulp.dest(staticDir + '/css'));
 
   gulp.src('scss/admin.scss')
-    .pipe(sass({
-      sourcemap: true
-    }))
+    .pipe(sass({ sourcemap: false }))
     .on('error', logError)
     .pipe(size({ showFiles: true }))
     .pipe(gulp.dest(staticDir + '/css'));
 });
 
+gulp.task('sass-dev', function() {
+  gulp.src('scss/main.scss')
+    .pipe(sass({ sourcemap: true }))
+    .on('error', logError)
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest(staticDir + '/css'));
+
+  gulp.src('scss/admin.scss')
+    .pipe(sass({ sourcemap: true }))
+    .on('error', logError)
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest(staticDir + '/css'));
+});
+
+// Watchers
 gulp.task('watch', function() {
-  gulp.watch('js/**/*', ['scripts'])
+  gulp.watch('js/**/*', ['scripts-dev'])
     .on('change', onChange);
 
-  gulp.watch('scss/**/*', ['sass'])
+  gulp.watch('scss/**/*', ['sass-dev'])
     .on('change', onChange);
 });
 
 gulp.task('default', ['dev']);
 
-gulp.task('dev', ['build', 'watch']);
+gulp.task('dev', ['build-dev', 'watch']);
 
-gulp.task('build', ['scripts', 'sass']);
+gulp.task('build-prod', ['scripts-prod', 'sass-prod']);
+
+gulp.task('build-dev', ['scripts-dev', 'sass-dev']);
