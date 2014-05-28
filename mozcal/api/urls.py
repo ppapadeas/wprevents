0a09f17860d767698559b8f5854d9222e3e9f37d
Gulthor <jbmusso+github@gmail.com>
2014-05-28 12:03:14 +0200

    Add Space and FunctionalArea API resources. Remove old CSV export.

--- mozcal/api/urls.py
+++ mozcal/api/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls.defaults import include, patterns, url
 
 from tastypie.api import Api
 
-from mozcal.events.api import EventResource
+from mozcal.events.api import EventResource, SpaceResource, FunctionalAreaResource
 
 
 v1_api = Api(api_name='v1')
 v1_api.register(EventResource())
+v1_api.register(SpaceResource())
+v1_api.register(FunctionalAreaResource())
 
 urlpatterns = patterns('', url(r'', include(v1_api.urls)))
--- mozcal/events/api.py
+++ mozcal/events/api.py
@@ -1,29 +1,60 @@
 from datetime import datetime
 from tastypie.resources import ModelResource
-from models import Event
+from models import Event, Space, FunctionalArea
 
 from mozcal.base.serializers import MozcalSerializer
 
 
-class EventResource(ModelResource):
+class MozcalResource(ModelResource):
+  def create_response(self, request, data, **response_kwargs):
+    """Add HTTP header to specify the filename of CSV exports."""
+    response = super(MozcalResource, self).create_response(request, data, **response_kwargs)
+
+    if self.determine_format(request) == 'text/csv':
+      today = datetime.now().date()
+
+      # Compute resource name from class name, ie. 'EventResource' -> 'event'
+      resource_name = self.__class__.__name__[:-8].lower()
+      filename = today.strftime(resource_name +'-export-%Y-%m-%d.csv')
+      response['Content-Disposition'] = 'filename="%s"' % filename
+
+    return response
+
+
+class EventResource(MozcalResource):
   class Meta:
     queryset = Event.objects.all()
+    fields = ['title', 'space', 'start', 'end', 'area_names']
     filtering = {
       "title": ('startswith',),
     }
+
     allowed_methods = ['get']
     include_resource_uri = False
     include_absolute_url = False
 
     serializer = MozcalSerializer(formats=['json', 'csv'])
 
-  def create_response(self, request, data, **response_kwargs):
-    """Add HTTP header to specify the filename of CSV exports."""
-    response = super(EventResource, self).create_response(request, data, **response_kwargs)
 
-    if self.determine_format(request) == 'text/csv':
-      today = datetime.now().date()
-      filename = today.strftime('events-export-%Y-%m-%d.csv')
-      response['Content-Disposition'] = 'filename="%s"' % filename
+class SpaceResource(MozcalResource):
+  class Meta:
+    queryset = Space.objects.all()
+    fields = ['name', 'address', 'address2', 'city', 'country', 'lat', 'lon', 'photo_url']
 
-    return response
+    allowed_methods = ['get']
+    include_resource_uri = False
+    include_absolute_url = False
+
+    serializer = MozcalSerializer(formats=['csv'])
+
+
+class FunctionalAreaResource(MozcalResource):
+  class Meta:
+    queryset = FunctionalArea.objects.all()
+    fields = ['name', 'slug', 'color']
+
+    allowed_methods = ['get']
+    include_resource_uri = False
+    include_absolute_url = False
+
+    serializer = MozcalSerializer(formats=['csv'])
