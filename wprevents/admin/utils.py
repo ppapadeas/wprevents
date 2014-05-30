070de95323a7cc3de80c4053ffac2e030442ccd1
Gulthor <jbmusso+github@gmail.com>
2014-05-30 15:49:17 +0200

    Implement basic import of Air Mozilla icalendar files

--- wprevents/admin/utils.py
+++ wprevents/admin/utils.py
@@ -1,25 +1,44 @@
 import urllib2
 
+from django.utils import timezone
 from icalendar import Calendar
 
-from wprevents.events.models import Event
+from wprevents.events.models import Event, Space
+
+default_timezone = timezone.get_default_timezone()
 
 
 def import_ical(url):
   request = urllib2.Request(url)
   response = urllib2.urlopen(request)
-
   data = response.read().decode('utf-8')
-
   data = data.replace(u"", "") # Temp fix for Mozilla remo ics file
 
   cal = Calendar.from_ical(data)
 
-  for event in cal.walk('VEVENT'):
-    model = Event(title="foo")
-    model.start = event.get('dtstart').dt
-    model.end = event.get('dtend').dt
-    model.title = event.get('summary')
-    model.description = event.get('description')
+  bulk_create_events(cal)
 
-  return
+
+def bulk_create_events(cal):
+  air_mozilla_space = Space.objects.get(name='Air Mozilla')
+  events = []
+
+  for ical_event in cal.walk('VEVENT'):
+    start = timezone.make_naive(ical_event.get('dtstart').dt, default_timezone)
+    end = timezone.make_naive(ical_event.get('dtend').dt, default_timezone)
+    title = ical_event.get('summary').encode('utf-8')
+
+    event = Event(
+      start = start,
+      end = end,
+      space = air_mozilla_space,
+      title = title,
+      description = ical_event.get('description').encode('utf-8')
+    )
+    # Generate slug because django's bulk_create() does not call Event.save(),
+    # which is where an Event's slug is normally set
+    event.define_slug()
+
+    events.append(event)
+
+  Event.objects.bulk_create(events)
