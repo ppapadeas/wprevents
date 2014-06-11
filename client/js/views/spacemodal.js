f62a57943a3ed0bd66b4aa3f76e2f11094a96dd8
Guigui <guillaume@littleworkshop.fr>
2014-06-11 16:44:46 +0200

    Show preview of photo in space modal

--- client/js/views/spacemodal.js
+++ client/js/views/spacemodal.js
@@ -4,11 +4,29 @@ var FormModalView = require('./formmodal');
 
 var SpaceModalView = FormModalView.extend({
   events: _.extend({}, FormModalView.prototype.events, {
-
+    'change .js-file-input': 'showPhotoPreview'
   }),
 
   initialize: function(options) {
     FormModalView.prototype.initialize.call(this);
+
+    this.$photo = this.$('.js-photo');
+  },
+
+  showPhotoPreview: function(e) {
+    this.readURL(e.target);
+  },
+
+  readURL: function(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        this.$photo.attr('src', e.target.result);
+      }.bind(this);
+
+      reader.readAsDataURL(input.files[0]);
+    }
   }
 });
 
