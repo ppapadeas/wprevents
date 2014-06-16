faa6f717886a3d6ac08927222a2445ff0820bb7a
Gulthor <jbmusso+github@gmail.com>
2014-06-16 19:57:31 +0200

    Add admin event delete/dedupe tests

--- wprevents/admin/tests/test_views.py
+++ wprevents/admin/tests/test_views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from wprevents.events.models import Event
+
 test_username = 'admin_tester'
 test_password = 'abcd1234'
 
@@ -21,6 +23,23 @@ class ViewsTest(TestCase):
     response = self.client.get(reverse('event_all'))
     self.assertTemplateUsed(response, 'events.html')
 
+    # Delete and dedupe
+  def test_view_admin_events_delete_logged_in(self):
+    """should delete/dedupe an event when logged in"""
+    self.client.login(username=test_username, password=test_password)
+    response = self.client.post(reverse('event_ajax_delete'), { 'id': 1 }, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(Event.objects.filter(pk=1).exists(), False)
+
+
+  def test_view_admin_events_delete_not_logged_out(self):
+    """should delete/dedupe an event when not logged in"""
+    response = self.client.post(reverse('event_ajax_delete'), { 'id': 1 }, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(Event.objects.filter(pk=1).exists(), True)
+
 
   # Spaces
   def test_view_admin_spaces(self):
@@ -38,5 +57,3 @@ class ViewsTest(TestCase):
 
     response = self.client.get(reverse('area_all'))
     self.assertTemplateUsed(response, 'areas.html')
-
-
