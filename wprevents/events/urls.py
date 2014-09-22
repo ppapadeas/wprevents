b71e423a4fae720c56faadc5d52a017c91bdb62d
sork <franck.git@gmail.com>
2014-09-22 19:36:56 +0200

    Remove obsolete url pattern

--- wprevents/events/urls.py
+++ wprevents/events/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.defaults import url, patterns
 from . import views
 
 urlpatterns = patterns('',
-  url(r'^testimport$', views.test_import, name='events_event_import'),
   url(r'^calendar$', views.calendar, name='events_event_calendar'),
   url(r'^filter_list$', views.filter_list, name='event_filter_list'),
   url(r'^filter_calendar$', views.filter_calendar, name='event_filter_calendar'),
